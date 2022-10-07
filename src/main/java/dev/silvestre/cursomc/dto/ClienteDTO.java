c6082e1805a4f4fa526b089a0ea069d916d4cdfe
Ivan Silvestre <isilvestreneto@gmail.com>
2022-10-07 15:13:13 -0300

    Email nao partido na atualizacao do cliente

--- src/main/java/dev/silvestre/cursomc/dto/ClienteDTO.java
+++ src/main/java/dev/silvestre/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import dev.silvestre.cursomc.domain.Cliente;
+import dev.silvestre.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
